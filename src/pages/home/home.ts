377985477ea8a370aefb5b245608efadc84c817e
Islam Mesha <eng.islammesha@gmail.com>
2018-08-11 00:52:31 +0200

    Send Http request using Ionic.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HTTP } from "@ionic-native/http";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor(public navCtrl: NavController) {
+  data: Array<string> = [];
+  imgUrl: string = "";
+  constructor(public navCtrl: NavController,
+    private http: HTTP) {
+
+    this.data = ['Islam', 'Muhammad', 'Hamed', 'Ibrahim', 'Mesha'];
+    this.imgUrl = "../../assets/imgs/logo.png";
+    console.log(navCtrl);
+
+  }
+
+  ngOnInit() {
+    console.log("I am Islam Mesha");
+    this.http.get('http://ionic.io', {}, {})
+      .then(data => {
+
+        console.log(data.status);
+        console.log(data.data); // data received by server
+        console.log(data.headers);
+
+      })
+      .catch(error => {
+
+        console.log(error.status);
+        console.log(error.error); // error message as string
+        console.log(error.headers);
+
+      });
 
   }
 
